c0e0efb7d342501f3f12232bbfd844afccf5759f
Keegawho198 <keegan.nair@hotmail.com>
2025-02-24 16:22:37 +1100

    Adding starting SQL explore code

--- SQL Analysis.sql
+++ SQL Analysis.sql
@@ -3,3 +3,16 @@
 
 SELECT *
 FROM customers;
+
+
+-- Customer Distribution
+-- How many customers from each country
+SELECT  Geography, COUNT(*) AS Total_customers
+FROM customers
+GROUP BY Geography;
+
+-- What is the gender breakdown of customers?
+SELECT  Gender, COUNT(*) AS Total_customers
+FROM customers
+GROUP BY Gender;
+
